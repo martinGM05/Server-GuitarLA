244464ef2e851f537330972f61f9fbed0b21a281
Martin Gonzalez <mtn.gm05@gmail.com>
2022-01-19 15:46:00 -0600

    Upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bdd334d55cf816d22733b99a97ee3041'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64e6bd92-1301-4605-9535-68c587c7602f'
+};
